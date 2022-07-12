a0123115d0642c9e808d84e53c9862742aa1f370
rnosach <iammo3r@gmail.com>
2022-07-12 11:11:54 +0300

    CoffeeMachine 1 stage

---
+++ coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
@@ -0,0 +1,15 @@
+package nix.summer.practice.coffeemachine
+
+fun main () {
+    stage1()
+}
+
+fun stage1 () {
+    println("Starting to make a coffee in NIX office\n" +
+            "Grinding coffee beans\n" +
+            "Boiling water\n" +
+            "Mixing boiled water with crushed coffee beans\n" +
+            "Pouring coffee into the cup\n" +
+            "Pouring some milk into the cup\n" +
+            "Coffee is ready! Go to work!")
+}
