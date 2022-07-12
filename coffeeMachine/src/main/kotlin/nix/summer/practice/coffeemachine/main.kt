b837c088a1868d71584dfa4f183c9ee56eeea3fa
rnosach <iammo3r@gmail.com>
2022-07-12 13:17:38 +0300

    CoffeeMachine 3 stage

--- coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
+++ coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
@@ -1,28 +1,33 @@
 package nix.summer.practice.coffeemachine
 
 fun main () {
-    stage1()
-    stage2()
+    stage3()
 }
 
-fun stage1 () {
-    println("Starting to make a coffee in NIX office\n" +
-            "Grinding coffee beans\n" +
-            "Boiling water\n" +
-            "Mixing boiled water with crushed coffee beans\n" +
-            "Pouring coffee into the cup\n" +
-            "Pouring some milk into the cup\n" +
-            "Coffee is ready! Go to work!")
-}
+fun stage3 () {
+    println("Write how many ml of water the coffee machine has:")
+    val waterHas = readLine()?.toInt()!!
+    println("Write how many ml of milk the coffee machine has:" )
+    val milkHas = readLine()?.toInt()!!
+    println("Write how many grams of coffee beans the coffee machine has:")
+    val beansHas = readLine()?.toInt()!!
 
-fun stage2 () {
     println("How many cups of coffee do you want?")
     val cups = readLine()?.toInt()!!
-    val water = cups * 200
-    val milk = cups * 50
-    val beans = cups * 15
-    println("For $cups cups of coffee you will need:\n" +
-            "$water ml of water\n" +
-            "$milk ml of milk\n" +
-            "$beans g of coffee beans")
+    val waterNeed = 200
+    val milkNeed = 50
+    val beansNeed = 15
+
+    if (cups > cups(waterHas,milkHas, beansHas)) println("No, I can make only ${cups(waterHas,milkHas, beansHas)} cups of coffee")
+    else if ((waterHas in waterNeed .. 399) and (milkHas in milkNeed .. 99) and (beansHas in beansNeed .. 29))
+        println("Yes, I can make that amount of coffee")
+    else if ((waterHas >= waterNeed*2) and (milkHas >= milkNeed*2) and (beansHas >= beansNeed*2))
+        println("Yes, I can make that amount of coffee (and even ${cups(waterHas,milkHas, beansHas) - cups} more than that)")
+}
+
+fun cups(per1: Int, per2: Int, per3: Int): Int {
+    val a = per1 / 200
+    val b = per2 / 50
+    val c = per3 / 15
+    return minOf(a, b, c)
 }
