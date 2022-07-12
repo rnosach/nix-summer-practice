2cf642ad0a53f238918d80204f2411658aa3002a
rnosach <iammo3r@gmail.com>
2022-07-12 14:45:16 +0300

    CoffeeMachine 4 stage

--- coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
+++ coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
@@ -1,33 +1,47 @@
 package nix.summer.practice.coffeemachine
 
 fun main () {
-    stage3()
+    stage4()
 }
 
-fun stage3 () {
-    println("Write how many ml of water the coffee machine has:")
-    val waterHas = readLine()?.toInt()!!
-    println("Write how many ml of milk the coffee machine has:" )
-    val milkHas = readLine()?.toInt()!!
-    println("Write how many grams of coffee beans the coffee machine has:")
-    val beansHas = readLine()?.toInt()!!
-
-    println("How many cups of coffee do you want?")
-    val cups = readLine()?.toInt()!!
-    val waterNeed = 200
-    val milkNeed = 50
-    val beansNeed = 15
-
-    if (cups > cups(waterHas,milkHas, beansHas)) println("No, I can make only ${cups(waterHas,milkHas, beansHas)} cups of coffee")
-    else if ((waterHas in waterNeed .. 399) and (milkHas in milkNeed .. 99) and (beansHas in beansNeed .. 29))
-        println("Yes, I can make that amount of coffee")
-    else if ((waterHas >= waterNeed*2) and (milkHas >= milkNeed*2) and (beansHas >= beansNeed*2))
-        println("Yes, I can make that amount of coffee (and even ${cups(waterHas,milkHas, beansHas) - cups} more than that)")
+fun stage4 () {
+    output(0,0,0,0,0)
+    when (readln()) {
+        "buy", "1" -> {
+            println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
+            when (readln()) {
+                "espresso", "1" -> output(250,0,16,1,-4)
+                "latte", "2" -> output(350,75,20,1,-7)
+                "cappuccino", "3" -> output(200,100,12,1,-6)
+            }
+        }
+        "fill", "2" -> {
+            println("Write how many ml of water you want to add:")
+            val waterGet = readLine()?.toInt()!!
+            println("Write how many ml of milk you want to add:")
+            val milkGet = readLine()?.toInt()!!
+            println("Write how many grams of coffee beans you want to add:")
+            val coffeeBeensGet = readLine()?.toInt()!!
+            println("Write how many disposable coffee cups you want to add:")
+            val cups = readLine()?.toInt()!!
+            output(-waterGet,-milkGet,-coffeeBeensGet,-cups,0)
+        }
+        "take", "3" -> output(0,0,0,0,550)
+    }
 }
 
-fun cups(per1: Int, per2: Int, per3: Int): Int {
-    val a = per1 / 200
-    val b = per2 / 50
-    val c = per3 / 15
-    return minOf(a, b, c)
+fun output(water:Int, milk:Int, coffeeBeans:Int, disposableCups:Int, money:Int) {
+    val waterHas = 400 - water
+    val milkHas = 540 - milk
+    val coffeeBeansHas = 120 - coffeeBeans
+    val disposableCupsHas = 9 - disposableCups
+    val moneyS = 550 - money
+
+    println("The coffee machine has:\n" +
+            "$waterHas of water\n" +
+            "$milkHas of milk\n" +
+            "$coffeeBeansHas of coffee beans\n" +
+            "$disposableCupsHas of disposable cups\n" +
+            "$moneyS of money\n\n" +
+            "Write action (buy, fill, take):")
 }
