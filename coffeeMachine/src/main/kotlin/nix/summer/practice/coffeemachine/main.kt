46202d02ea1ad8e1530238c74426fdff488dc533
rnosach <iammo3r@gmail.com>
2022-07-14 13:11:36 +0300

    CoffeeMachine 5 stage

--- coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
+++ coffeeMachine/src/main/kotlin/nix/summer/practice/coffeemachine/main.kt
@@ -1,47 +1,78 @@
 package nix.summer.practice.coffeemachine
 
 fun main () {
-    stage4()
+    stage5()
 }
 
-fun stage4 () {
-    output(0,0,0,0,0)
+fun stage5 () {
+    var waterHas = 400
+    var milkHas = 540
+    var coffeeBeansHas = 120
+    var disposableCupsHas = 9
+    var money = 550
+    output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
     when (readln()) {
         "buy", "1" -> {
             println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:")
-            when (readln()) {
-                "espresso", "1" -> output(250,0,16,1,-4)
-                "latte", "2" -> output(350,75,20,1,-7)
-                "cappuccino", "3" -> output(200,100,12,1,-6)
+                when (readln()) {
+
+                "espresso", "1" -> {
+                    waterHas -= 250
+                    coffeeBeansHas -= 16
+                    disposableCupsHas -= 1
+                    money += 4
+                    output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
+                }
+                "latte", "2" -> {
+                    waterHas -= 300
+                    milkHas -= 75
+                    coffeeBeansHas -= 20
+                    disposableCupsHas -= 1
+                    money += 7
+                    output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
+                }
+                "cappuccino", "3" -> {
+                    waterHas -= 200
+                    milkHas -= 100
+                    coffeeBeansHas -= 12
+                    disposableCupsHas -= 1
+                    money += 6
+                    output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
+                }
+                else -> {}
             }
         }
         "fill", "2" -> {
             println("Write how many ml of water you want to add:")
             val waterGet = readLine()?.toInt()!!
+            waterHas += waterGet
             println("Write how many ml of milk you want to add:")
             val milkGet = readLine()?.toInt()!!
+            milkHas += milkGet
             println("Write how many grams of coffee beans you want to add:")
             val coffeeBeensGet = readLine()?.toInt()!!
+            coffeeBeansHas += coffeeBeensGet
             println("Write how many disposable coffee cups you want to add:")
             val cups = readLine()?.toInt()!!
-            output(-waterGet,-milkGet,-coffeeBeensGet,-cups,0)
+            disposableCupsHas += cups
+            output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
         }
-        "take", "3" -> output(0,0,0,0,550)
+        "take", "3" -> {
+            println("I gave you $money")
+            money -= money
+            output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
+        }
+        "remaining"-> {
+            output(waterHas,milkHas,coffeeBeansHas,disposableCupsHas,money)
+        }
     }
 }
-
 fun output(water:Int, milk:Int, coffeeBeans:Int, disposableCups:Int, money:Int) {
-    val waterHas = 400 - water
-    val milkHas = 540 - milk
-    val coffeeBeansHas = 120 - coffeeBeans
-    val disposableCupsHas = 9 - disposableCups
-    val moneyS = 550 - money
-
     println("The coffee machine has:\n" +
-            "$waterHas of water\n" +
-            "$milkHas of milk\n" +
-            "$coffeeBeansHas of coffee beans\n" +
-            "$disposableCupsHas of disposable cups\n" +
-            "$moneyS of money\n\n" +
+            "$water of water\n" +
+            "$milk of milk\n" +
+            "$coffeeBeans of coffee beans\n" +
+            "$disposableCups of disposable cups\n" +
+            "$money of money\n\n" +
             "Write action (buy, fill, take):")
 }
